8ad6b6d3579ab28be52ff00465d9853928483d13
Alex Verboon <alex.verboon@basevision.ch>
2019-11-06 22:02:23 +0100

    added stuff

---
+++ PowerShell/lastconnected.ps1
@@ -0,0 +1,3 @@
+﻿$reg = "HKLM:\Software\Microsoft\Windows Advanced Threat Protection\Status"
+$lastconnected = Get-itempropertyvalue $reg -Name LastConnected
+[datetime]::fromFiletime([int64]::parse($lastconnected))
