daa0145dfabf2a02ef9de522b4583560cfdf6f74
Alex Verboon <alex.verboon@basevision.ch>
2020-11-01 20:51:21 +0100

    added demo samples

---
+++ LiveResponse/Library/Scripts/Run-scquery.ps1
@@ -0,0 +1,21 @@
+﻿Function Run-scquery
+{
+<#
+.Synopsis
+    Executes sc querywitin an MDATP Live response session
+
+.Description
+    Executes sc query witin an MDATP Live response session
+
+    Within an MDATP Live Response session run the following commands to download the content to the machine
+    putfile Run-scquery.ps1
+
+    run the following command witin the live response session to execute the sript
+    run Run-sqquery.ps1
+
+#>
+Start-Process sc -ArgumentList "query" -NoNewWindow
+}
+
+## Run it
+Run-scquery
