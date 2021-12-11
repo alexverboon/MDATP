db16137a0f0b294f33832614b5bd1ee64514083c
alex verboon <verboonalex@gmail.com>
2021-12-11 14:32:55 +0100

    get office documents list in live response session

---
+++ LiveResponse/Library/Scripts/Get-OfficeDocuments.ps1
@@ -0,0 +1,18 @@
+﻿
+Function Get-OfficeDocuments(){
+
+$Drives = Get-PSDrive | Select-Object -ExpandProperty 'Name' | Select-String -Pattern '^[a-z]$'
+$FileInfo = [System.Collections.ArrayList]::new()
+$Filetypes =  @("*.docx","*.pptx","*.xlsx","*.pdf","*.doc","*.xls")
+ForEach($DriveLetter in $Drives)
+{
+    $Drive = "$DriveLetter" + ":\"
+    $DriveFiles = Get-ChildItem -Path $Drive -Include $Filetypes -Recurse -File -ErrorAction SilentlyContinue | Select-Object FullName
+    [void]$FileInfo.Add($DriveFiles)
+    }
+$FileInfo
+}
+Get-OfficeDocuments
+
+
+
