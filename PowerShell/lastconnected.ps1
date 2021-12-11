23e985e4197adfce926b5bf23efb57da3cad1765
alex verboon <verboonalex@gmail.com>
2021-12-11 14:33:08 +0100

    MDE last connected

--- PowerShell/lastconnected.ps1
+++ PowerShell/lastconnected.ps1
@@ -1,3 +1,21 @@
-﻿$reg = "HKLM:\Software\Microsoft\Windows Advanced Threat Protection\Status"
+﻿# Last MDE Connection Timestamp
+$reg = "HKLM:\Software\Microsoft\Windows Advanced Threat Protection\Status"
 $lastconnected = Get-itempropertyvalue $reg -Name LastConnected
+[datetime]::fromFiletime([int64]::parse($lastconnected))
+
+
+# Last connection to MDE Channel - attempt
+$reg = "HKLM:\SOFTWARE\Microsoft\SenseCM"
+$lastconnected = Get-itempropertyvalue $reg -Name LastCheckinAttempt
+[datetime]::fromFiletime([int64]::parse($lastconnected))
+
+# Last connection to MDE Channel - success
+$reg = "HKLM:\SOFTWARE\Microsoft\SenseCM"
+$lastconnected = Get-itempropertyvalue $reg -Name LastCheckinSuccess
+[datetime]::fromFiletime([int64]::parse($lastconnected))
+
+
+# maps
+$reg = "HKLM:\SOFTWARE\Microsoft\Windows Defender\Spynet"
+$lastconnected = Get-itempropertyvalue $reg -Name LastMAPSSuccessTime
 [datetime]::fromFiletime([int64]::parse($lastconnected))
